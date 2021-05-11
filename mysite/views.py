ad3da99039a574881cd8001e953bcbbbab0fdc2e
邱姵瑄 <ymes050623@gmail.com>
2021-05-11 15:28:04 +0800

    add index_html

--- mysite/views.py
+++ mysite/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 import random
 from mysite.models import Post
 
-
 def index(request):
 	posts = Post.objects.all()
 	myname = "球球"
@@ -12,3 +11,10 @@ def index(request):
 	lotto_numbers = data[0:6]
 	special_number = data[6]
 	return render(request,'index.html',locals())
+
+def show(request, id):
+	try:
+		target = Post.objects.get(id=id)
+	except:
+		target = None
+	return render(request,"showpost.html", locals())
